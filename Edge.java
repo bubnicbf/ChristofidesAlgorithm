d7c75a9ff9d0d896146415c265d3899ed89a48d5
Ben Bubnick <ben.bubnick@gmail.com>
2015-04-04 10:34:47 -0400

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,22 @@
+public class Edge implements Comparable {
+    int from, to;
+    double cost;
+
+    public Edge(int _from, int _to, double _cost) {
+				from = _from;
+				to = _to;
+				cost = _cost;
+    }
+
+    public int compareTo(Object _o) {
+				Edge e = (Edge)_o;
+				if(this.cost == e.cost) return 0;
+				else if(this.cost > e.cost) return 1;
+				else return -1;
+    }
+
+    public int getTo() {return to;}
+
+
+    public int getFrom() {return from;}
+}
